5139f65df86e01004e5f3d6614272cee6180e99d
jjh770 <159859972+jjh770@users.noreply.github.com>
2024-08-12 10:22:46 +0900

    [Silver III] Title: 소수 구하기, Time: 2944 ms, Memory: 12792 KB -BaekjoonHub

---
+++ C#/백준/Silver/1929. 소수 구하기/소수 구하기.cs
@@ -0,0 +1,29 @@
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = input[0], m = input[1];
+
+            bool[] isPrime = new bool[m + 1];
+            Array.Fill(isPrime, true);
+            isPrime[0] = isPrime[1] = false;
+
+            for (int i = 2; i * i <= m; i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = i * i; j <= m; j += i)
+                        isPrime[j] = false;
+                }
+            }
+            for (int i = n; i <= m; i++)
+            {
+                if (isPrime[i])
+                    Console.WriteLine(i + " ");
+            }
+        }
+    }
+}

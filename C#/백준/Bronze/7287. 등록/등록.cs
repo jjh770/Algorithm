e258795dc20082344fc8f8c05089058e27ec5a2e
jjh770 <159859972+jjh770@users.noreply.github.com>
2024-08-12 17:00:56 +0900

    [Bronze V] Title: 등록, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ C#/백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,11 @@
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("69"); 
+            Console.WriteLine("ahricon");
+        }
+    }
+}

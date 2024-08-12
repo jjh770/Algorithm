94b8a95f3cc555cfa0cc9006cd12570b4ae8c420
jjh770 <159859972+jjh770@users.noreply.github.com>
2024-08-12 16:57:41 +0900

    [Bronze V] Title: ??!, Time: 48 ms, Memory: 5376 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,11 @@
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input+"??!");
+        }
+    }
+}

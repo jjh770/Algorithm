cf8d03055f625fd3f3553cdc874f1afdee6fb2a9
jjh770 <159859972+jjh770@users.noreply.github.com>
2025-10-27 13:47:07 +0900

    [Bronze V] Title: 사칙연산, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        string[] parts = input.Split(' ');
+
+        int a = int.Parse(parts[0]);
+        int b = int.Parse(parts[1]);
+
+        Console.WriteLine(a + b);
+        Console.WriteLine(a - b);
+        Console.WriteLine(a * b);
+        Console.WriteLine(a / b);
+        Console.WriteLine(a % b);
+
+    }
+}

deda3e1e4600fc87c17d362a3311fc4f11e23062
jjh770 <159859972+jjh770@users.noreply.github.com>
2025-10-28 16:41:49 +0900

    [Bronze V] Title: 문자와 문자열, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ C#/백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,9 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(input[int.Parse(Console.ReadLine()) - 1]);
+    }
+}

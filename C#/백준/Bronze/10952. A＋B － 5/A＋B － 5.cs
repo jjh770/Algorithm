1b15030a6a39aa21b34f11a6480e9e1fe1f3405c
jjh770 <159859972+jjh770@users.noreply.github.com>
2025-10-28 10:13:54 +0900

    [Bronze V] Title: A+B - 5, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine(sb);
+                return;
+            }
+
+            sb.Append(a + b + "\n");
+        }
+    }
+}

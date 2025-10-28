501aad2cec40111c112b26f01faa55c20be1957f
jjh770 <159859972+jjh770@users.noreply.github.com>
2025-10-28 17:44:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5436 KB -BaekjoonHub

---
+++ C#/백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        char asc = input[0];
+        Console.WriteLine((int)asc);
+    }
+}

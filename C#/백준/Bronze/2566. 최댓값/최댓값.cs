9400a4dc8d6d8185ba55c752676887a25049056c
jjh770 <159859972+jjh770@users.noreply.github.com>
2025-11-03 10:17:58 +0900

    [Bronze III] Title: 최댓값, Time: 52 ms, Memory: 5784 KB -BaekjoonHub

---
+++ C#/백준/Bronze/2566. 최댓값/최댓값.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maxInt = int.MinValue;
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 9; i++)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int[] intInput = input.Select(int.Parse).ToArray();
+            for(int j = 0; j < intInput.Length; j++)
+            {
+                if (intInput[j] > maxInt)
+                {
+                    row = i;
+                    col = j;
+                    maxInt = intInput[j];
+                }
+            }
+        }
+        Console.WriteLine(maxInt);
+        Console.WriteLine($"{row + 1} {col + 1}");
+    }
+}
